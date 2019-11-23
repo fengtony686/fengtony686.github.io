63fe79abc2cbf71ca53c796829a7d700a4d7b260
Tony Feng <fengtony686@gmail.com>
2019-11-23 10:31:55 +0800

    add bird

---
+++ games/Bird/js/global.js
@@ -0,0 +1,20 @@
+require.register('global.js', function(exports, require, module) {
+'use strict';
+
+exports.font = '"Segoe UI", "Microsoft YaHei", 宋体, sans-serif';
+
+exports.settings = {
+  gravity: 40,
+  flap: 620,
+  speed: 390,
+  spacing: 468,
+  gap: 300,
+  ceiling: false,
+  canvas: true,
+  antialias: false,
+  debug: false,
+  scoreSounds: 43,
+  hurtSounds: 21
+};
+
+});
